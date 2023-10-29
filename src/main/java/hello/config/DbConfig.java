c6ba32fe609bcd47afabf1a011fd7f9bea3be5e4
g1kwon <spring_shot@naver.com>
2023-10-29 23:47:20 +0900

    check autoConfiguration in Springboot

--- src/main/java/hello/config/DbConfig.java
+++ src/main/java/hello/config/DbConfig.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.TransactionManager;
 import javax.sql.DataSource;
 
 @Slf4j
-@Configuration
+//@Configuration springboot 자동 구성
 public class DbConfig {
 
     @Bean
